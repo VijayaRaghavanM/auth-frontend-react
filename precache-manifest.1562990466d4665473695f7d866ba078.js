fcdcdd1dc37e46c888c1b775869391340ec256b0
Vijaya Raghavan M <mvijayaraghavan29@gmail.com>
2019-06-30 15:51:28 +0530

    Updates

---
+++ precache-manifest.1562990466d4665473695f7d866ba078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af484673167e75e2738054cd8394673",
+    "url": "/auth-frontend-react/index.html"
+  },
+  {
+    "revision": "4d2fd935e35509e73d96",
+    "url": "/auth-frontend-react/static/css/main.52457415.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/auth-frontend-react/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "4d2fd935e35509e73d96",
+    "url": "/auth-frontend-react/static/js/main.813b0bfb.chunk.js"
+  },
+  {
+    "revision": "641af72aa45de8c9ecbe",
+    "url": "/auth-frontend-react/static/js/runtime~main.244b752f.js"
+  }
+]);
